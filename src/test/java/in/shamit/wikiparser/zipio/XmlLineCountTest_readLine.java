d2261d7b57cc41ad10f0bdae85d6211c1de5681c
Shamit Verma <github@shamit.in>
2020-06-14 16:06:34 +0530

    Fixed warnings

--- src/test/java/in/shamit/wikiparser/zipio/XmlLineCountTest_readLine.java
+++ src/test/java/in/shamit/wikiparser/zipio/XmlLineCountTest_readLine.java
@@ -4,16 +4,17 @@ import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class XmlLineCountTest_readLine {
-    public static void main(String args[]) throws Exception{
+    public static void main(String[] args) {
         String  archivePath = "K:\\tmp\\enwiki-latest-pages-articles.xml";
         try(InputStream fin = Files.newInputStream(Paths.get(archivePath));
             //BufferedInputStream in = new BufferedInputStream(fin,1024*1024*1024);
             BufferedInputStream in = new BufferedInputStream(fin);
-            BufferedReader reader = new BufferedReader(new InputStreamReader(in,"UTF-8"))){
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8))){
                 long count=0;
                 //long count=reader.lines().count();
                 while(reader.readLine()!=null){
@@ -26,7 +27,7 @@ public class XmlLineCountTest_readLine {
     }
 }
 
-/** Results
+/* Results
  *Run 	0	00:04:36
  * Run 	1	00:04:37
  * Run 	2	00:04:30
