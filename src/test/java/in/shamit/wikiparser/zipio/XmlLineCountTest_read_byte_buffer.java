f14487de8e0d1d5fc7569e3f78d3c2fafa42d369
shamit <github@shamit.in>
2020-06-27 12:53:39 +0530

    Read test as bytes (instead of characters)

---
+++ src/test/java/in/shamit/wikiparser/zipio/XmlLineCountTest_read_byte_buffer.java
@@ -0,0 +1,45 @@
+package in.shamit.wikiparser.zipio;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class XmlLineCountTest_read_byte_buffer {
+    public static void main(String[] args) {
+        String  archivePath = "F:\\nlp\\Wikipedia\\wiki_aug_2019\\enwiki-latest-pages-articles-multistream.xml";
+        int buf_size=1000*1000*100;
+        try(InputStream fin = Files.newInputStream(Paths.get(archivePath));){
+            long count=0;
+            int read;
+            byte []buf;
+            while((buf=fin.readNBytes(buf_size)).length>0){
+                read=buf.length;
+                for(int i=0;i<read;i++){
+                    if(buf[i]=='\r'||buf[i]=='\n'){
+                        count++;
+                    }
+                }
+            }
+            System.out.println(count);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
+
+/* Results
+0
+Run 	0	00:04:15
+0
+Run 	1	00:03:21
+0
+Run 	2	00:03:24
+0
+Run 	3	00:03:25
+Average 	 	00:03:36
+ *
+ *
+ */
